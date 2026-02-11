3ac7efb9b4b11eef9a37bdd962fed71f19b859cd
hopemedhm-bit <hopemed.hm@gmail.com>
2026-02-11 12:51:05 +0530

    Update paymentRoutes.js

--- routes/paymentRoutes.js
+++ routes/paymentRoutes.js
@@ -8,21 +8,11 @@ const {
 } = require("../controllers/paymentController");
 
 
-// ============================================================
-// 1️⃣ Create Dynamic QR (Android → Backend)
-// ============================================================
+
 router.post("/create", createPayment);
 
-
-// ============================================================
-// 2️⃣ Check Payment Status (Android Polling)
-// ============================================================
 router.get("/status/:orderId", checkStatus);
 
-
-// ============================================================
-// 3️⃣ PSP / Manual Status Update Webhook (Optional Admin Tool)
-// ============================================================
 router.post("/webhook", updateStatus);
 
 
