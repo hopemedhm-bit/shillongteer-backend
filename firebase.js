7ddb60b5e0b65ffc0b7b81f47dabfd36edd7c9d0
hopemedhm-bit <hopemed.hm@gmail.com>
2026-02-13 14:34:30 +0530

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,15 @@
+const admin = require("firebase-admin");
+
+if (!admin.apps.length) {
+    admin.initializeApp({
+        credential: admin.credential.cert({
+            projectId: process.env.GOOGLE_PROJECT_ID,
+            clientEmail: process.env.GOOGLE_CLIENT_EMAIL,
+            privateKey: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+        }),
+    });
+
+    console.log("🔥 Firebase Admin Initialized");
+}
+
+module.exports = admin;
