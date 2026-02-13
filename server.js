f56c9a3a23f66926d26e282a7aa397559d8d646c
hopemedhm-bit <hopemed.hm@gmail.com>
2026-02-13 14:35:14 +0530

    Update server.js

--- server.js
+++ server.js
@@ -13,13 +13,6 @@ dotenv.config();
 // ===============================
 // INIT FIREBASE ADMIN (FCM)
 // ===============================
-admin.initializeApp({
-    credential: admin.credential.cert({
-        projectId: process.env.GOOGLE_PROJECT_ID,
-        clientEmail: process.env.GOOGLE_CLIENT_EMAIL,
-        privateKey: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n')
-    })
-});
 
 // ===============================
 // CONNECT DATABASE
